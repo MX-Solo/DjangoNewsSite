c1158283693c50c5896e0a87012d96abeb8104a3
iman <imanhasankhani1@gmail.com>
2024-12-15 01:11:52 +0330

    create model and structer

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import News,Category,Comment
+
+#  Home Page
+def home(request):
+    pass
+
+# All News Page
+def all_news(request):
+    pass
+
+# Deatil Page
+def detail(request,id):
+    pass
+
+#fetch all category
+def all_category(request):
+    pass
+
+#fetch news in category
+def category(request,id):
+    pass
